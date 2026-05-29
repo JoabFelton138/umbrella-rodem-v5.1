85407466c33a021c554e3812a525e7d5a0ea6e26
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-05-29 14:51:18 +0100

    feat: if small screen, display error message

--- app/components/desktop/Desktop.tsx
+++ app/components/desktop/Desktop.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { LabViewWindow } from "./lab-view/LabViewWindow";
 import styles from "./Desktop.module.css";
 import { DesktopIcons } from "./desktop-icons/DesktopIcons";
@@ -5,17 +6,33 @@ import { DataFile } from "./data-file/DataFile";
 import { RegistryWindow } from "./registry-window/RegistryWindow";
 import { RepeatSystem } from "./repeat-system/RepeatSystem";
 import { Error } from "../error-dialogue/Error";
+import { useEffect, useState } from "react";
 
 export const Desktop = () => {
+    const [isMobile, setIsMobile] = useState(false);
+    const errorMessage = "Error: Recalibrate your system before continuing.";
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setIsMobile(window.innerWidth < 900);
+        });
+    }, []);
+
     return (
         <div className={styles.desktop}>
-            <DesktopIcons />
-            {/* <RegistryWindow /> */}
-            <div className={styles.labCluster}>
-                <LabViewWindow />
-                <RepeatSystem />
-            </div>
-            <DataFile />
+      
+            {isMobile ? 
+                <Error message={errorMessage}/> 
+                : 
+                <>
+                    <DesktopIcons />
+                    <RegistryWindow />
+                    <div className={styles.labCluster}>
+                        <LabViewWindow />
+                        <RepeatSystem />
+                    </div>
+                    <DataFile />
+                </>}
         </div>
     );
 };
